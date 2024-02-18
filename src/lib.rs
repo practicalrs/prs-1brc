6b0fb58fd8b88dfbc1660dbf855983f13dcd2846
Michal Piotrowski <practicalrs.com@gmail.com>
2024-02-19 00:51:34 +0100

    #2 initial implementation p2

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use std::{env, fs::File, io::BufReader, io::prelude::*};
+use std::{collections::BTreeMap, env, fs::File, io::BufReader, io::prelude::*};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[derive(Debug)]
 pub enum Error {
     CommandLine,
+    Parsing,
 }
 
 impl std::error::Error for Error {}
@@ -12,6 +13,18 @@ impl std::error::Error for Error {}
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Error!")
+    }
+}
+
+struct Db {
+    data: BTreeMap<String, Vec<f32>>,
+}
+
+impl Db {
+    fn new() -> Self {
+        Self {
+            data: BTreeMap::new(),
+        }
     }
 }
 
@@ -25,9 +38,18 @@ pub fn run() -> Result<()> {
     let mut line = String::new();
 
     let mut i = 1;
+
+    let mut db = Db::new();
+
     loop {
         let len = reader.read_line(&mut line)?;
 
+        println!("{line}");
+        let values = line.strip_suffix('\n').ok_or(Error::Parsing)?.split(';').collect::<Vec<&str>>();
+        println!("values = {:?}", values);
+        let name = values.first().ok_or(Error::Parsing)?;
+        let value = values.last().ok_or(Error::Parsing)?.parse::<f32>()?;
+        println!("name = {name}, value = {value}");
         println!("i = {i} {len}");
         i += 1;
 
